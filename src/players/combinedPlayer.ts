14f43c31197131ac5eadae6e8888ffe0a237ecb1
Liam Moynihan <liammoyn@gmail.com>
2024-12-30 11:23:25 -0600

    Reuse context aware code for combined player

--- src/players/combinedPlayer.ts
+++ src/players/combinedPlayer.ts
@@ -1,6 +1,7 @@
 import { Board, Move, Player, Space } from "../minesweeper/types";
 import { getAdjacentTs, isNeighborS } from "../utils/gridUtils";
 import { isSame } from "../utils/spaceUtils";
+import { tryToFindMoveForEdge } from "./contextAwarePlayerV2";
 import cspPlayer from "./cspPlayer";
 
 /**
@@ -10,104 +11,6 @@ import cspPlayer from "./cspPlayer";
 const combinedPlayer = (setHighlights: boolean, delayMs: number): Player => {
     const cspPlayerInst = cspPlayer(false, 0)
     let potentialMoves: Space[] = []
-
-    function tryToFindMoveForSpace(space: Space, candidateNeighbors: Space[], knownBombNumber: number): Move | null {
-        let nextMove: Move | null = null
-        if (candidateNeighbors.length === 0) { return nextMove }
-        if (space.bombsNear === candidateNeighbors.length + knownBombNumber) {
-            // All unopened spaces must be bombs, since this on edge there is at least one unflagged one
-            const nextNeighbor = candidateNeighbors.find(un => !un.isFlagged)
-            nextMove = nextNeighbor != null ? { coord: { row: nextNeighbor.row, col: nextNeighbor.col }, action: "FLAG" } : null
-        } else if (space.bombsNear === knownBombNumber) {
-            // All unopened and unflagged spaces must be safe
-            const nextNeighbor = candidateNeighbors.find(un => !un.isFlagged)
-            nextMove = nextNeighbor != null ? { coord: { row: nextNeighbor.row, col: nextNeighbor.col }, action: "POP" } : null
-        }
-        return nextMove
-    }
-
-    function tryToFindMoveForEdge(board: Board, numbersOnEdge: Set<Space>): Move | null {
-        let nextMove: Move | null = null
-        let spaceToCandidates: Map<Space, Space[]> = new Map()
-        let spaceToFlags: Map<Space, number> = new Map()
-
-        for (let space of numbersOnEdge) {
-            const coord = { row: space.row, col: space.col }
-            const nonEmptyNeighbors = getAdjacentTs(coord, board.grid, s => !(s.isOpen && s.bombsNear === 0))
-            const candidateNeighbors = nonEmptyNeighbors.filter(s => !s.isOpen && !s.isFlagged)
-            const flaggedNeighbors = nonEmptyNeighbors.filter(s => s.isFlagged)
-            nextMove = tryToFindMoveForSpace(space, candidateNeighbors, flaggedNeighbors.length)
-            if (nextMove != null) {
-                return nextMove;
-            }
-
-            spaceToCandidates.set(space, candidateNeighbors)
-            spaceToFlags.set(space, flaggedNeighbors.length)
-        }
-
-
-        for (let space of numbersOnEdge) {
-            // For each space, get a list of numbers who's candidates are all adjacent to this space.
-            const theseCandidates = spaceToCandidates.get(space)!!
-            const possibleNumbers = new Set(
-                theseCandidates.flatMap(candidate => getAdjacentTs(
-                    candidate,
-                    board.grid,
-                    s => numbersOnEdge.has(s) && !isSame(s, space)
-                ))
-            )
-            const usefulNumbers = Array.from(possibleNumbers).filter(s => {
-                const thoseCandidates = spaceToCandidates.get(s)
-                return thoseCandidates?.every(c => isNeighborS(space, c))
-            })
-
-            // For each  of usefulNumbers, attempt to reprocess this space's candidates that aren't shared with number
-            for (let numberSpace of usefulNumbers) {
-                const eligibleSpaces = theseCandidates.filter(c => !isNeighborS(c, numberSpace))
-                const thisFlags = spaceToFlags.get(space)!!
-                const extraBombs = numberSpace.bombsNear - spaceToFlags.get(numberSpace)!!
-                nextMove = tryToFindMoveForSpace(space, eligibleSpaces, thisFlags + extraBombs)
-                if (nextMove !== null) {
-                    return nextMove
-                }
-            }
-
-            // For each permutation of usefulNumbers who's candidate numbers are distinct, attempt to reprocess
-            // this space's candidates that aren't shared with any number in the permutation
-            // TODO: This is slow
-            const noSharedCandidates = (s1: Space, s2: Space) => {
-                const c1s = spaceToCandidates.get(s1)!!
-                const c2s = spaceToCandidates.get(s2)!!
-                return c1s.every(c1 => c2s.every(c2 => !isSame(c1, c2)))
-            }
-            const permutations: Space[][] = []
-            for (let i = 0; i < usefulNumbers.length; i++) {
-                const thisNumber = usefulNumbers[i]
-                const newPermutations: Space[][] = [[thisNumber]]
-                for (let j = 0; j < permutations.length; j++) {
-                    const thisPerm = permutations[j]
-                    if (thisPerm.every(s => noSharedCandidates(s, thisNumber))) {
-                        newPermutations.push([...thisPerm, thisNumber])
-                    }
-                }
-                permutations.push(...newPermutations)
-            }
-
-            for (let permutation of permutations) {
-                if (permutation.length == 1) { continue; }
-                const eligibleSpaces = theseCandidates.filter(c => permutation.every(numberSpace => !isNeighborS(numberSpace, c)))
-                const thisFlags = spaceToFlags.get(space)!!
-                const extraBombs = permutation.reduce((acc, cur) => acc + (cur.bombsNear - spaceToFlags.get(cur)!!), 0)
-                nextMove = tryToFindMoveForSpace(space, eligibleSpaces, thisFlags + extraBombs)
-                if (nextMove !== null) {
-                    return nextMove
-                }
-            }
-        }
-
-        return null
-    }
-
 
     return {
         newGame: (board: Board): Promise<void> => {
